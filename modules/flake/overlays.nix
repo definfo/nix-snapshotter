07ded6467d09b56abf7a9c35932198ec91bcdc08
bgl gwyng <bgl@gwyng.com>
2026-01-18 00:24:50 +0900

    Remove k3s override

--- modules/flake/overlays.nix
+++ modules/flake/overlays.nix
@@ -2,7 +2,7 @@
 {
   # Provide overlay to add `nix-snapshotter`.
   flake.overlays.default = self: super: {
-    containerd-1_7 = super.containerd.overrideAttrs(_: rec {
+    containerd-1_7 = super.containerd.overrideAttrs (_: rec {
       version = "1.7.28";
 
       src = self.fetchFromGitHub {
@@ -31,27 +31,16 @@
     nix-snapshotter = self.callPackage ../../package.nix {
       inherit (inputs) globset;
     };
-
-    k3s = super.k3s_1_30.override {
-      buildGoModule = args: super.buildGoModule (args // super.lib.optionalAttrs (args.pname != "k3s-cni-plugins" && args.pname != "k3s-containerd") {
-        vendorHash = {
-          "sha256-qEvdBT3noOtKdIdHDJZChowXzQMpVpY/l1ioTJCGVJ4=" = "sha256-fwhwoK+ID4BZtI6cRUQjkR9w2IVpaCrYLrfy8+irq5w=";
-        }.${args.vendorHash};
-        # Source https://patch-diff.githubusercontent.com/raw/k3s-io/k3s/pull/9319.patch
-        # Remove when merged
-        patches = (args.patches or []) ++ [
-          ./patches/k3s-nix-snapshotter.patch
-        ];
-      });
-    };
   };
 
-  perSystem = { system, ... }: {
-    _module.args.pkgs = import inputs.nixpkgs {
-      inherit system;
-      # Apply default overlay to provide nix-snapshotter for NixOS tests &
-      # configurations.
-      overlays = [ self.overlays.default ];
+  perSystem =
+    { system, ... }:
+    {
+      _module.args.pkgs = import inputs.nixpkgs {
+        inherit system;
+        # Apply default overlay to provide nix-snapshotter for NixOS tests &
+        # configurations.
+        overlays = [ self.overlays.default ];
+      };
     };
-  };
 }
